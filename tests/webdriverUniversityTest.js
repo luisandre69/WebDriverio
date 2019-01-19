66cd9e649c205ba58fd11e4509114a4e9d9a48fc
luisandre69 <luisandre_69@hotmail.com>
2019-01-19 20:37:30 +0000

    Node Assertions
    Node Assertions

--- tests/webdriverUniversityTest.js
+++ tests/webdriverUniversityTest.js
@@ -1,20 +1,24 @@
+var assert = require('assert');
+
 describe("Verify whether webdriveruniversity links on homepage work correctly", function () {
 	it("check that the contact us button opens the contact us page", function (done) {
 		browser.setViewportSize({
 			width: 1200,
 			height: 800
 		})
-		browser.url('http://www.webdriveruniversity.com/')
+		browser.url('/')
 		var title = browser.getTitle();
+		assert.equal(title,'WebDriverUniversity.com');
 		console.log('Title is: ' + title);
 		browser.click("#contact-us")
 		browser.pause(3000)
 	});
-
+	
 	it("check that the login button opens the login portal page", function (done) {
-		browser.url('http://www.webdriveruniversity.com/')
+		browser.url('/')
 		browser.click('#login-portal')
 		var title = browser.getTitle()
+		assert.equal(title,'WebDriverUniversity.com');
 		console.log('Title is: ' + title);
 		browser.pause(3000)
 	});
