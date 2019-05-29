1f9dfdaacf36ce5475a0c5324ce4e02bf82aacc3
Luis Abreu <luisandre_69@hotmail.com>
2019-05-29 23:43:22 +0100

    fixed #4

--- tests/videoExecuteCommandTest.js
+++ tests/videoExecuteCommandTest.js
@@ -2,23 +2,30 @@ beforeEach(function() {
 	browser.url("https://www.w3schools.com/html/html5_video.asp");
 })
 
-//Injects a snippet of JavaScript into the page for execution in the context of the currently selected frame.
 browser.addCommand('isVideoPaused', function(){
 	var isPaused = browser.execute(function(){
-        console.log('Outputted inside the console window');
+		console.log('Outputted inside the console window');
 
-        var video = document.querySelector('#video1');
-        return video.paused;
-    })
-    return isPaused.value;
+		var video = document.querySelector('#video1');
+		return video.paused;
+	})
+	return isPaused.value;
 });
 
 describe('Video test', function() {
-	it('Validate that the video is paused when accessing the page', function(done) {
-
+	it('Validate that the video is paused upon accessing the page', function(done) {
+		this.timeout(20000);
+		browser.pause(6000);
+		var isPaused = browser.isVideoPaused();
+		expect(isPaused).to.be.true;
+		browser.pause(6000);
 	});
 
 	it('Alter the width of the video', function(done) {
-
+		var videoWidth = browser.execute(function() {
+			var video = document.querySelector('#video1');
+			return video.style.width = "300px";
+		});
+		browser.pause(6000);		
 	});
 });
