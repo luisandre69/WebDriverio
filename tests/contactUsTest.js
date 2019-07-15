0277244f2f2a4aaec282b4e8af53aa651ba86606
Luis Abreu <luisandre_69@hotmail.com>
2019-07-15 22:53:02 +0100

    77

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,6 +1,5 @@
 var request = require('sync-request');
 
-
 beforeEach(function () {
     browser.url('/Contact-Us/contactus.html');
 })
@@ -13,7 +12,7 @@ describe('Test Contact Us form WebdriverUni', function () {
     var lastNameSelector = "[name='last_name']";
     var emailAddressSelector = "[name='email']";
     var commentsSelector = "textarea";
-    var sucessfullSubmissionSelector = "#contact_reply h1";
+    var successfulSubmissionSelector = "#contact_reply h1";
     var unsuccessfulSubmissionSelector = "body";
     var submitButtonSelector = "[type='submit']";
 
@@ -21,7 +20,7 @@ describe('Test Contact Us form WebdriverUni', function () {
         return browser.setValue(firstNameSelector, firstName);
     }
 
-    function setlastName(lastName) {
+    function setLastName(lastName) {
         return browser.setValue(lastNameSelector, lastName);
     }
 
@@ -39,53 +38,52 @@ describe('Test Contact Us form WebdriverUni', function () {
 
     function confirmSuccessfulSubmission() {
         var validateSubmissionHeader = browser.waitUntil(function () {
-            return browser.getText(sucessfullSubmissionSelector) == 'Thank You for your Message!';
+            return browser.getText(successfulSubmissionSelector) == 'Thank You for your Message!'
         }, 3000)
-        expect(validateSubmissionHeader, 'Successful submission Message does not Exist!').to.be.true;
+        expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
     }
+
+    function confirmUnsuccessfulSubmission() {
+        var validateSubmissionHeader = browser.waitUntil(function () {
+            return browser.getText(unsuccessfulSubmissionSelector) == 'Error: all fields are required'
+        }, 3000)
+        expect(browser.getText(unsuccessfulSubmissionSelector)).to.include('Error: all fields are required');
+    }
+
 
 
     contactusDetails.forEach(function (contactDetail) {
         it('Should be able to submit a successful submission via contact us form', function (done) {
-            browser.submitDataViaContactUsForm('joe', 'Blogs', contactDetail.email, contactDetail.body);
-
-            var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-            expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.true;
-
-            var successfulSubmission = browser.getText('#contact_reply h1');
-            expect(successfulSubmission).to.equal('Thank You for your Message!');
-        })
-    });
-
-    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-        browser.setValue("[name='first_name']", 'Mike');
-        browser.setValue("[name='last_name']", 'Woods');
-        browser.setValue("[name='email']", 'mike_woods@mail.com');
-        browser.click("[type='submit']");
-
-        var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-        expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
+            setFirstName('joe');
+            setLastName('Blogs');
+            setEmailAddress(contactDetail.email);
+            setComments(contactDetail.body);
+            clickSubmitButton();
+            confirmSuccessfulSubmission();
+        });
     });
 
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-        browser.setValue("[name='first_name']", 'Sarah');
-        browser.setValue("[name='email']", 'sarah_woods@mail.com');
-        browser.click("[type='submit']");
+        setFirstName('Mike');
+        setLastName('Woods');
+        setEmailAddress('mike_woods@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSubmission();
+    });
 
-        var successfulContactConfirmation = browser.isExisting('#contact_reply h1');
-        expect(successfulContactConfirmation, 'Successful submission Message does not exist').to.be.false;
+
+    it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
+        setFirstName('Sarah');
+        setEmailAddress('sarah_woods@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSubmission();
     });
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
-        browser.setValue("[name='first_name']", 'Jim');
-        browser.setValue("[name='last_name']", 'Jomes');
-        browser.click("[type='submit']");
-
-        var errorText = browser.getText('body');
-        expect(errorText).to.include('Error: all fields are required');
-
-        var errorText = browser.isVisible('body');
-        expect(errorText, 'Error message is not visible').to.be.true;
+        setLastName('Jomes');
+        setEmailAddress('sarah_Jomes@mail.com');
+        clickSubmitButton();
+        confirmUnsuccessfulSubmission();
     });
 });
