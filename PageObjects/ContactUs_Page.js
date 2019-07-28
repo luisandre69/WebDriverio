6c6136752dd7631236b263b03a60d1e820cdec40
Luis Abreu <luisandre_69@hotmail.com>
2019-07-28 22:58:53 +0100

    85/86

--- PageObjects/ContactUs_Page.js
+++ PageObjects/ContactUs_Page.js
@@ -1,11 +1,45 @@
-class ContactUs_Page{
-    get firstName() { return $("[name='first_name']");}
-    get lastName() { return $("[name='last_name']");}
-    get comments() { return $("textarea");}
-    get emailAddress() { return $("[name='email']");}
-    get submitButton() { return $("[type='submit']");}
-    get successfulSubmissionHeader() { return $("#contact_reply h1");}
-    get unsuccessfulSubmissionHeader() { return $("body");}
+class ContactUs_Page {
+	get firstName() { return $("[name='first_name']");}
+	get lastName() { return $("[name='last_name']");}
+	get comments() { return $("textarea");}
+	get emailAddress() { return $("[name='email']");}
+	get submitButton() { return $("[type='submit']");}
+	get successfulSubmissionHeader() { return $("#contact_reply h1");}
+	get unsuccessfulSubmissionHeader() { return $("body");}
 }
+
+function setFirstName(firstName) {
+		return this.firstName.setValue(firstName);
+	}
+
+	function setLastName(lastName) {
+		return this.lastName.setValue(lastName);
+	}
+
+	function setEmailAddress(emailAddress) {
+		return this.emailAddress.setValue(emailAddress);
+	}
+
+	function setComments(comments) {
+		return this.comments.setValue(comments);
+	}
+
+	function clickSubmitButton() {
+		return this.submitButton.click();
+	}
+
+	function confirmSuccessfulSubmission() {
+		var validateSubmissionHeader = browser.waitUntil(function() {
+			return this.successfulSubmissionHeader.getText() == 'Thank You for your Message!'
+		}, 3000)
+		expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
+	}
+
+	function confirmUnsuccessfulSubmission() {
+		var validateSubmissionHeader = browser.waitUntil(function() {
+			return this.unsuccessfulSubmissionHeader.getText() == 'Error: all fields are required'
+		}, 3000)
+		expect(this.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
+	}
 
 module.exports = new ContactUs_Page();
--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -1,53 +1,19 @@
-var ContactUs_Page = require("../PageObjects/ContactUs_Page.js");
+var ContactUs_Page = require("../pageObjects/ContactUs_Page.js");
 
 beforeEach(function () {
     browser.url('/Contact-Us/contactus.html');
 })
 
 describe('Test Contact Us form WebdriverUni', function () {
-
-    function setFirstName(firstName) {
-        return ContactUs_Page.firstName.setValue(firstName);
-    }
-
-    function setLastName(lastName) {
-        return ContactUs_Page.lastName.setValue(lastName);
-    }
-
-    function setEmailAddress(emailAddress) {
-        return ContactUs_Page.emailAddress.setValue(emailAddress);
-    }
-
-    function setComments(comments) {
-        return ContactUs_Page.comments.setValue(comments);
-    }
-
-    function clickSubmitButton() {
-        return ContactUs_Page.submitButton.click();
-    }
-
-    function confirmSuccessfulSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function () {
-            return ContactUs_Page.successfulSubmissionHeader.getText() == 'Thank You for your Message!'
-        }, 3000)
-        expect(validateSubmissionHeader, 'Successful Submission Message does not Exist!').to.be.true;
-    }
-
-    function confirmUnsuccessfulSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function () {
-            return ContactUs_Page.unsuccessfulSubmissionHeader.getText() == 'Error: all fields are required'
-        }, 3000)
-        expect(ContactUs_Page.unsuccessfulSubmissionHeader.getText()).to.include('Error: all fields are required');
-    }
-
     it('Should be able to submit a successful submission via contact us form', function (done) {
         setFirstName('joe');
         setLastName('Blogs');
-        setEmailAddress("joe_blogs123@outlook.com");
-        setComments("How are you?");
+        setEmailAddress('joe_blogs123@outlook.com');
+        setComments('How are you?');
         clickSubmitButton();
         confirmSuccessfulSubmission();
     });
+
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
         setFirstName('Mike');
@@ -56,6 +22,7 @@ describe('Test Contact Us form WebdriverUni', function () {
         clickSubmitButton();
         confirmUnsuccessfulSubmission();
     });
+
 
     it('Should not be able to submit a successful submission via contact us form as all fields are required', function (done) {
         setFirstName('Sarah');
